bcb9befcf193fbf72cc40f5dd6dcb81e9bb2fce1
KilianBrd <kilianberaud3@gmail.com>
2023-10-11 09:40:22 +0200

    First

--- src/models/Animal.ts
+++ src/models/Animal.ts
@@ -3,15 +3,15 @@ import mongoose, { Schema, Document } from 'mongoose';
 // Schéma  pour l'animal
 const AnimalSchema = new Schema({
     nom: { type: String, required: true },
-    espece: { type: String, required: true },
-      // un animal peut être attaché à zero ou plusieurs employés
-    employes: [{ type: Schema.Types.ObjectId, ref: 'Employe' }]    
+    espece: [{ type: Schema.Types.ObjectId, ref: 'Espece', require: true }],
+    // un animal peut être attaché à zero ou plusieurs employés
+    employes: [{ type: Schema.Types.ObjectId, ref: 'Employe' }]
 });
 
 // Modèle pour l'animal
 export interface AnimalModel extends Document {
     nom: string;
-    espece: string;
+    espece: mongoose.Types.ObjectId;
     employes: string[];
 }
 
